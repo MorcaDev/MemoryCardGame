5079d7d9fd208e845790147e52b109a8fa45b382
MorcaDev <morca225@gmail.com>
2021-05-12 22:33:15 -0500

    Charts terminados (adpatados con heights de prueba)

---
+++ script.js
@@ -0,0 +1,23 @@
+const button1stChart    = document.getElementById("button1stChart");
+const button2ndChart    = document.getElementById("button2ndChart");
+const button3rdChart    = document.getElementById("button3rdChart");
+
+let chart1 = document.getElementById("chartOne");
+let chart2 = document.getElementById("chartTwo");
+let chart3 = document.getElementById("chartThree");
+
+button1stChart.addEventListener("click",close1);
+
+function close1(ev){
+    chart1.classList.add("Disapper");
+    button2ndChart.addEventListener("click",close2);
+}
+
+function close2(ev){
+    chart2.classList.add("Disapper");
+    button3rdChart.addEventListener("click",close3);
+}
+
+function close3(ev){
+    chart3.classList.add("Disapper");
+}
